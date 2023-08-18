b164c66d18afb5d24bda1c0f6142ef81871b5a7d
Kamogelo Maponya <maponyakamogelo@gmail.com>
2023-08-18 19:34:05 +0200

    Starting

--- Tetris_CloneGroup9/Assets/MyScripts/Blocks.cs
+++ Tetris_CloneGroup9/Assets/MyScripts/Blocks.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BricMovement : MonoBehaviour
+public class Blocks : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
