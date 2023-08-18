3b6985160ae27e43c0b531a94eb403b0feb6b947
Erin Harper <erinharper24@gmail.com>
2023-08-18 14:34:14 +0200

    Wall Kicks
    -so that object does not go out of the board

--- Tetris_CloneGroup9/Assets/Scripts/Piece.cs
+++ Tetris_CloneGroup9/Assets/Scripts/Piece.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -16,6 +17,7 @@ public class Piece : MonoBehaviour
         this.board = board;
         this.position = position;
         this.data = data;   
+
         this.rotationIndex = 0;
 
         if(this.cells == null)
@@ -41,7 +43,6 @@ public class Piece : MonoBehaviour
         {
             Rotate(1);
         }
-
 
             if (Input.GetKeyDown(KeyCode.A)) 
         {
@@ -91,15 +92,27 @@ public class Piece : MonoBehaviour
 
     private void Rotate(int direction)
     {
-        this.rotationIndex += Wrap(this.rotationIndex + direction, 0, 4);
+        int originalRotation = this.rotationIndex;
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+        ApplyRotationMatrix(direction);
 
-        for(int i = 0; i < this.cells.Length; i++)
+        if(!TestWallKicks(this.rotationIndex, direction))
+        {
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction)
+    {
+
+        for (int i = 0; i < this.cells.Length; i++)
         {
             Vector3 cell = this.cells[i];
 
             int x, y;
 
-            switch (this.data.tetromino) 
+            switch (this.data.tetromino)
             {
                 case Tetromino.I:
                 case Tetromino.O:
@@ -118,6 +131,35 @@ public class Piece : MonoBehaviour
 
             this.cells[i] = new Vector3Int(x, y, 0);
         }
+    }
+
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+
+        for (int i = 0; i < this.data.wallKicks.GetLength(1); i++)
+        {
+            Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
+
+            if (Move(translation))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+
+        if(rotationDirection < 0)
+        {
+            wallKickIndex--;
+        }
+
+        return Wrap(wallKickIndex, 0, this.data.wallKicks.GetLength(0));
     }
 
     private int Wrap(int input, int min, int max)
